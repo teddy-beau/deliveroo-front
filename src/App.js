5e5e1e2674ad48064b1d631ff7a4cd056909f348
teddy-beau <hello@teddy-beau.com>
2021-02-08 14:27:34 +0100

    added fetchData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-   return <div>Hello</div>;
+   const [data, setData] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
+
+   const fetchData = async () => {
+      try {
+         const response = await axios.get("http://localhost:3000/");
+         setData(response.data);
+         setIsLoading(false);
+      } catch (error) {
+         alert("An error occured!");
+      }
+   };
+
+   useEffect(() => fetchData(), []);
+
+   return isLoading ? (
+      <span>En cours de chargement...</span>
+   ) : (
+      <div>{data.restaurant.name}</div>
+   );
 }
 
 export default App;
